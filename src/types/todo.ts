f5aa82491788367deb2470f4ce98c7fdc1a272aa
willantunes <antunes406@gmail.com>
2026-06-21 20:00:46 +0200

    feat: adiciona tipos de domínio de todo
    iFactory plan 8957fa01-3b5f-4da3-9b7d-23700ad0ca00

---
+++ src/types/todo.ts
@@ -0,0 +1,15 @@
+export interface Todo {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CreateTodoInput {
+  title: string;
+}
+
+export interface TodoListResponse {
+  items: Todo[];
+}
