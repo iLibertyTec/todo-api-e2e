ce3132deeb46d98f34d31b7e7dfd4db66aac9d08
willantunes <antunes406@gmail.com>
2026-06-21 20:30:15 +0200

    feat: adiciona store em memória de todos
    iFactory plan cc28b2a7-96d9-430c-b8e1-66a19274dd76

---
+++ src/todos/memoryTodoStore.ts
@@ -0,0 +1,80 @@
+import {
+  type CreateTodoInput,
+  type Todo,
+  type UpdateTodoInput,
+} from "./types.ts";
+
+export class MemoryTodoStore {
+  #todos: Todo[] = [];
+  #nextId: number;
+
+  constructor(initialTodos: Todo[] = []) {
+    this.#todos = initialTodos.map((todo: Todo): Todo => cloneTodo(todo));
+    this.#nextId = initialTodos.length + 1;
+  }
+
+  list(): Todo[] {
+    return this.#todos.map((todo: Todo): Todo => cloneTodo(todo));
+  }
+
+  create(input: CreateTodoInput): Todo {
+    const now = new Date().toISOString();
+    const todo: Todo = {
+      id: String(this.#nextId++),
+      title: input.title,
+      completed: false,
+      createdAt: now,
+      updatedAt: now,
+    };
+
+    this.#todos.push(todo);
+
+    return cloneTodo(todo);
+  }
+
+  getById(id: string): Todo | undefined {
+    const todo = this.#todos.find((item: Todo): boolean => item.id === id);
+    return todo ? cloneTodo(todo) : undefined;
+  }
+
+  update(id: string, input: UpdateTodoInput): Todo | undefined {
+    const index = this.#todos.findIndex((todo: Todo): boolean => todo.id === id);
+
+    if (index === -1) {
+      return undefined;
+    }
+
+    const current = this.#todos[index];
+    const updated: Todo = {
+      ...current,
+      ...(input.title !== undefined ? { title: input.title } : {}),
+      ...(input.completed !== undefined ? { completed: input.completed } : {}),
+      updatedAt: new Date().toISOString(),
+    };
+
+    this.#todos[index] = updated;
+
+    return cloneTodo(updated);
+  }
+
+  delete(id: string): boolean {
+    const index = this.#todos.findIndex((todo: Todo): boolean => todo.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    this.#todos.splice(index, 1);
+    return true;
+  }
+}
+
+function cloneTodo(todo: Todo): Todo {
+  return {
+    id: todo.id,
+    title: todo.title,
+    completed: todo.completed,
+    createdAt: todo.createdAt,
+    updatedAt: todo.updatedAt,
+  };
+}
